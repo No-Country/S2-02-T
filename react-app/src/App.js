eef39dbffcb3ae05a0a6d3f166980b23e35e13ce
Richard Miguel Irala <richardirala47@hotmail.com>
2022-05-31 19:17:17 -0300

    Footer provicional hecho y css del navbar ajustado

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,8 +1,14 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Navbar from "./components/Navbar/Navbar.jsx";
+import Footer from "./components/Footer/Footer.jsx";
 function App() {
-  return <Navbar />;
+  return (
+    <>
+      <Navbar />
+      <Footer />
+    </>
+  );
 }
 
 export default App;
