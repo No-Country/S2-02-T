0ab69af7c2a348a985eccf86be77e8cffde7f268
Julián Perna <julianperna@gmail.com>
2022-05-31 20:50:47 -0300

    creación de componente hero

---
+++ react-app/src/components/Hero/Hero.js
@@ -0,0 +1,17 @@
+import "./styles.css"
+
+const Hero = () => {
+  return (  
+    <>
+    <h1 className="hero__title">Landing page UI kit</h1>
+    <p className="hero__subtitle">Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+    </p>
+    <div className="hero__button">Get Started</div>
+    <div className="hero__image">
+      <img  src="landing.png" alt="" />
+    </div>
+    </>
+  );
+}
+ 
+export default Hero;
