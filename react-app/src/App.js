bbd2f8b7d1076abb3a69d8f36c96be3a73760613
Richard Miguel Irala <richardirala47@hotmail.com>
2022-06-01 15:34:11 -0300

    Steps Component creado y imagenes agregadas a imgs

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -2,10 +2,12 @@ import logo from "./logo.svg";
 import "./App.css";
 import Navbar from "./components/Navbar/Navbar.jsx";
 import Footer from "./components/Footer/Footer.jsx";
+import Steps from "./components/Steps/Steps";
 function App() {
   return (
     <>
       <Navbar />
+      <Steps />
       <Footer />
     </>
   );
