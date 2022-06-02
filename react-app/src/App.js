22c218ac71756ff2acf827db7eff8079e64688c5
Oscar <43421688+dert98@users.noreply.github.com>
2022-06-02 10:45:45 -0300

    prueba

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,13 +1,13 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Cuerpo from './components/Cuerpo/Cuerpo';
-import Inicio from './components/Inicio/Inicio';
+import Tabla from './components/Tabla/Tabla';
 import Login from './components/Login/Login';
 import Pie from './components/Pie/Pie';
 
 function App() {
   return (
-    <><Cuerpo /><Inicio /><Login /><Pie name="Vane" /></>
+    <><Cuerpo /><Tabla /><Login /><Pie name="Vane" /></>
   );
 }
 
