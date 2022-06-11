f75eaac4100a7611a174334bb0cede6eb7e93619
Julián Perna <julianperna@gmail.com>
2022-06-11 00:06:47 -0300

    se modificó el componente hero

--- react-app/src/components/Hero/Hero.js
+++ react-app/src/components/Hero/Hero.js
@@ -4,12 +4,12 @@ const Hero = () => {
   return (  
     <>
     <div className="hero__container">
-    <h1 className="hero__title">Gastify App</h1>
+    <h1 className="hero__title">Digital Finance</h1>
     <p className="hero__subtitle">La mejor calculadora para simular tus gastos. Facil y rápido!
     </p>
     <div className="hero__button">Probar</div>
     <div className="hero__image">
-      <img  src="landing.png" alt="" />
+      <img   className="hero__calculator" src="calcu-money.jpeg" alt="" />
     </div>
     <div className="hero__circle">
     </div>
