965c553b6551fd0ba794d427a5c659fe8e566078
Richard Miguel Irala <richardirala47@hotmail.com>
2022-06-11 13:45:32 -0300

    Componente Calculadora iniciado con un mockup temporal parte 2

---
+++ react-app/src/components/Calculadora/Calculadora.jsx
@@ -0,0 +1,75 @@
+import React from "react";
+import "./Calculadora.css";
+
+export default class Calculadora extends React.Component {
+  componentDidMount() {}
+  render() {
+    return (
+      <>
+        <div className="display--flex f-dir-column align--center">
+          <div>
+            <h1>Calculadora de amortización de crédito</h1>
+          </div>
+          {/*Inputs o formulario */}
+          <div className="calc-container">
+            <form className="calc-form-mip">
+              <label>
+                <span>Monto del crédito</span>
+                <input placeholder="$" />
+              </label>
+              <label>
+                <span>Tasa de interes anual</span>
+                <input placeholder="%" />
+              </label>
+              <div className="calc-form-mip__pea">
+                <label>
+                  <span>Plazo en años</span>
+                  <input placeholder="$" />
+                </label>
+                {/* <div>
+                  <div>+</div>
+                  <div>-</div>
+                </div> */}
+              </div>
+            </form>
+            {/* RESULTADOS */}
+            <div className="calc-container__results-container">
+              <h1>Resultados</h1>
+              <div className="calc-container__results">
+                <div className="calc-container__result-r">
+                  <span>Pago mensual</span>
+                  <img
+                    src="money_billete.svg"
+                    alt=""
+                    className="calc-container__result-r-img"
+                  />
+                  <span className="calc-container__result-r__m">$482.02</span>
+                </div>
+                <div className="calc-container__result-r">
+                  <span>Intereses totales</span>
+                  <img
+                    src="money_billete.svg"
+                    alt=""
+                    className="calc-container__result-r-img"
+                  />
+                  <span className="calc-container__result-r__m">$1568.50</span>
+                </div>
+                <div className="calc-container__result-r">
+                  <span>Pago total</span>
+                  <img
+                    src="money_billete.svg"
+                    alt=""
+                    className="calc-container__result-r-img"
+                  />
+                  <span className="calc-container__result-r__m">
+                    $11,568.50
+                  </span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </>
+    );
+  }
+}
