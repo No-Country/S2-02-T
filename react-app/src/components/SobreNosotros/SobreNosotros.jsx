62eb55dfffd87b332f5797b91968d9bf551172c8
F4cku23 <f4cku23.n@outlook.es>
2022-06-16 23:26:10 -0300

    se creo el apartado nosotros, faltan cambios

--- react-app/src/components/SobreNosotros/SobreNosotros.jsx
+++ react-app/src/components/SobreNosotros/SobreNosotros.jsx
@@ -5,14 +5,121 @@ class SobreNosotros extends React.Component {
   render() {
     return (
       <>
-        <h1>Sobre Nosotros - <small class="text-muted">Equipo: </small></h1>
+        <div className="container-fluid">
+          <div className="row">
+            <div className="col-1"></div>
+              <div className="col-md-10">
+              <br /><br />
+                <h1>Sobre Nosotros - <small className="text-muted">Equipo S2-02-T: </small></h1>
+                <div className="container"><br/>
+                  <div className="row">
+                <div className="col-md-4">
+                  <div className="card">
+                      <img className="card-img-top" src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fpinguinodigital.com%2Fwp-content%2Fuploads%2F2020%2F08%2Fpagina-en-construcci%25C3%25B3n4-1536x864.png&f=1&nofb=1" alt=""/>
+                      <div className="card-body">
+                      <h5 className="card-title text-xl-center">Paulo Nievas - Backend</h5>
+                      <div className="row">
+                      <div className="col"></div>
+                        <div className="col">
+                          <a href="https://www.linkedin.com"><img src="linkedin.png" alt="" className=" position-relative"/></a>
+                        </div>
+                        <div className="col">
+                            <a href="https://www.github.com"><img src="github.png" alt="" className=" position-relative"/></a>
+                        </div>
+                        <div className="col"></div>
+                      </div>
+                      </div>
+                  </div>
+              </div>
+              <div className="col-md-4">
+                  <div className="card">
+                      <img className="card-img-top" src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fpinguinodigital.com%2Fwp-content%2Fuploads%2F2020%2F08%2Fpagina-en-construcci%25C3%25B3n4-1536x864.png&f=1&nofb=1" alt=""/>
+                      <div className="card-body">
+                      <h5 className="card-title text-xl-center">Orlando - Backend</h5>
+                      <div className="row">
+                      <div className="col"></div>
+                        <div className="col">
+                          <a href="https://www.linkedin.com"><img src="linkedin.png" alt="" className=" position-relative"/></a>
+                        </div>
+                        <div className="col">
+                            <a href="https://www.github.com"><img src="github.png" alt="" className=" position-relative"/></a>
+                        </div>
+                        <div className="col"></div>
+                      </div>
+                      </div>
+                  </div>
+              </div>
+              <div className="col-md-4">
+                  <div className="card">
+                      <img className="card-img-top" src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fpinguinodigital.com%2Fwp-content%2Fuploads%2F2020%2F08%2Fpagina-en-construcci%25C3%25B3n4-1536x864.png&f=1&nofb=1" alt=""/>
+                      <div className="card-body">
+                      <h5 className="card-title text-xl-center">Oscar - Backend</h5>
+                      <div className="row">
+                      <div className="col"></div>
+                        <div className="col">
+                          <a href="https://www.linkedin.com"><img src="linkedin.png" alt="" className=" position-relative"/></a>
+                        </div>
+                        <div className="col">
+                            <a href="https://www.github.com"><img src="github.png" alt="" className=" position-relative"/></a>
+                        </div>
+                        <div className="col"></div>
+                      </div>
+                      </div>
+                  </div>
+              </div>
+              </div>
+              <div className="col-1"></div>
+            </div>
 
-        <div class="social">
-          <ul>
-            
-            <li><a href="mailto:contacto@falconmasters.com" class="icon-mail"></a></li>
-          </ul>
-      </div>
+            <div className="container"><br/>
+                  <div className="row">
+                  <div className="col-2"></div>
+                <div className="col-md-4">
+                  <div className="card">
+                      <img className="card-img-top" src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fpinguinodigital.com%2Fwp-content%2Fuploads%2F2020%2F08%2Fpagina-en-construcci%25C3%25B3n4-1536x864.png&f=1&nofb=1" alt=""/>
+                      <div className="card-body">
+                      <h5 className="card-title text-xl-center">Julian - Frontend</h5>
+                      <div className="row">
+                      <div className="col"></div>
+                        <div className="col">
+                          <a href="https://www.linkedin.com"><img src="linkedin.png" alt="" className=" position-relative"/></a>
+                        </div>
+                        <div className="col">
+                            <a href="https://www.github.com"><img src="github.png" alt="" className=" position-relative"/></a>
+                        </div>
+                        <div className="col"></div>
+                      </div>
+                      </div>
+                  </div>
+              </div>
+              <div className="col-md-4">
+                  <div className="card">
+                      <img className="card-img-top" src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fpinguinodigital.com%2Fwp-content%2Fuploads%2F2020%2F08%2Fpagina-en-construcci%25C3%25B3n4-1536x864.png&f=1&nofb=1" alt=""/>
+                      <div className="card-body">
+                      <h5 className="card-title text-xl-center">Richard - Frontend</h5>
+                      <div className="row">
+                      <div className="col"></div>
+                        <div className="col">
+                          <a href="https://www.linkedin.com"><img src="linkedin.png" alt="" className=" position-relative"/></a>
+                        </div>
+                        <div className="col">
+                            <a href="https://www.github.com"><img src="github.png" alt="" className=" position-relative"/></a>
+                        </div>
+                        <div className="col"></div>
+                      </div>
+                      </div>
+                  </div>
+              </div>
+              
+              </div>
+              <div className="col-2"></div>
+            </div>
+
+
+          </div>
+        </div>
+        <br /><br /><br />
+        </div>
       </>
     );
   }
