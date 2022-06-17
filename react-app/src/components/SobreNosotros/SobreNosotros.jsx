b155e7044ec10e83a039af0677a11587f01e6530
F4cku23 <f4cku23.n@outlook.es>
2022-06-17 20:44:09 -0300

    se agrego el contenido en las card

--- react-app/src/components/SobreNosotros/SobreNosotros.jsx
+++ react-app/src/components/SobreNosotros/SobreNosotros.jsx
@@ -15,16 +15,16 @@ class SobreNosotros extends React.Component {
                   <div className="row">
                 <div className="col-md-4">
                   <div className="card">
-                      <img className="card-img-top" src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fpinguinodigital.com%2Fwp-content%2Fuploads%2F2020%2F08%2Fpagina-en-construcci%25C3%25B3n4-1536x864.png&f=1&nofb=1" alt=""/>
+                      <img className="card-img-top" src="paulo_nievas.jfif" alt="perfil"/>
                       <div className="card-body">
                       <h5 className="card-title text-xl-center">Paulo Nievas - Backend</h5>
                       <div className="row">
                       <div className="col"></div>
                         <div className="col">
-                          <a href="https://www.linkedin.com"><img src="linkedin.png" alt="" className=" position-relative"/></a>
+                          <a href="https://www.linkedin.com/in/paulo-facundo-gaston-nievas/" target="_blank"><img src="linkedin.png" alt="" className=" position-relative"/></a>
                         </div>
                         <div className="col">
-                            <a href="https://www.github.com"><img src="github.png" alt="" className=" position-relative"/></a>
+                            <a href="https://github.com/F4cku23" target="_blank"><img src="github.png" alt="" className=" position-relative"/></a>
                         </div>
                         <div className="col"></div>
                       </div>
@@ -33,16 +33,16 @@ class SobreNosotros extends React.Component {
               </div>
               <div className="col-md-4">
                   <div className="card">
-                      <img className="card-img-top" src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fpinguinodigital.com%2Fwp-content%2Fuploads%2F2020%2F08%2Fpagina-en-construcci%25C3%25B3n4-1536x864.png&f=1&nofb=1" alt=""/>
+                      <img className="card-img-top" src="orlando_neris.jfif" alt="perfil"/>
                       <div className="card-body">
                       <h5 className="card-title text-xl-center">Orlando - Backend</h5>
                       <div className="row">
                       <div className="col"></div>
                         <div className="col">
-                          <a href="https://www.linkedin.com"><img src="linkedin.png" alt="" className=" position-relative"/></a>
+                          <a href="https://www.linkedin.com/in/orlandoneris823/" target="_blank"><img src="linkedin.png" alt="" className=" position-relative"/></a>
                         </div>
                         <div className="col">
-                            <a href="https://www.github.com"><img src="github.png" alt="" className=" position-relative"/></a>
+                            <a href="https://github.com/OlandoNeris" target="_blank"><img src="github.png" alt="" className=" position-relative"/></a>
                         </div>
                         <div className="col"></div>
                       </div>
@@ -51,16 +51,16 @@ class SobreNosotros extends React.Component {
               </div>
               <div className="col-md-4">
                   <div className="card">
-                      <img className="card-img-top" src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fpinguinodigital.com%2Fwp-content%2Fuploads%2F2020%2F08%2Fpagina-en-construcci%25C3%25B3n4-1536x864.png&f=1&nofb=1" alt=""/>
+                      <img className="card-img-top" src="oscar_ruiz.jfif" alt="perfil"/>
                       <div className="card-body">
                       <h5 className="card-title text-xl-center">Oscar - Backend</h5>
                       <div className="row">
                       <div className="col"></div>
                         <div className="col">
-                          <a href="https://www.linkedin.com"><img src="linkedin.png" alt="" className=" position-relative"/></a>
+                          <a href="https://www.linkedin.com/in/oscar-ruiz-7a3371171/" target="_blank"><img src="linkedin.png" alt="" className=" position-relative"/></a>
                         </div>
                         <div className="col">
-                            <a href="https://www.github.com"><img src="github.png" alt="" className=" position-relative"/></a>
+                            <a href="https://github.com/dert98" target="_blank"><img src="github.png" alt="" className=" position-relative"/></a>
                         </div>
                         <div className="col"></div>
                       </div>
@@ -76,16 +76,16 @@ class SobreNosotros extends React.Component {
                   <div className="col-2"></div>
                 <div className="col-md-4">
                   <div className="card">
-                      <img className="card-img-top" src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fpinguinodigital.com%2Fwp-content%2Fuploads%2F2020%2F08%2Fpagina-en-construcci%25C3%25B3n4-1536x864.png&f=1&nofb=1" alt=""/>
+                      <img className="card-img-top" src="julian_perna.jfif" alt="perfil"/>
                       <div className="card-body">
                       <h5 className="card-title text-xl-center">Julian - Frontend</h5>
                       <div className="row">
                       <div className="col"></div>
                         <div className="col">
-                          <a href="https://www.linkedin.com"><img src="linkedin.png" alt="" className=" position-relative"/></a>
+                          <a href="https://www.linkedin.com/in/julián-perna-5b7508136/" target="_blank"><img src="linkedin.png" alt="" className=" position-relative"/></a>
                         </div>
                         <div className="col">
-                            <a href="https://www.github.com"><img src="github.png" alt="" className=" position-relative"/></a>
+                            <a href="https://github.com/julianperna" target="_blank"><img src="github.png" alt="" className=" position-relative"/></a>
                         </div>
                         <div className="col"></div>
                       </div>
@@ -94,16 +94,16 @@ class SobreNosotros extends React.Component {
               </div>
               <div className="col-md-4">
                   <div className="card">
-                      <img className="card-img-top" src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fpinguinodigital.com%2Fwp-content%2Fuploads%2F2020%2F08%2Fpagina-en-construcci%25C3%25B3n4-1536x864.png&f=1&nofb=1" alt=""/>
+                      <img className="card-img-top" src="richard_irala.jfif" alt="perfil"/>
                       <div className="card-body">
                       <h5 className="card-title text-xl-center">Richard - Frontend</h5>
                       <div className="row">
                       <div className="col"></div>
                         <div className="col">
-                          <a href="https://www.linkedin.com"><img src="linkedin.png" alt="" className=" position-relative"/></a>
+                          <a href="https://www.linkedin.com/in/richard-miguel-irala-5139a2217/" target="_blank"><img src="linkedin.png" alt="" className=" position-relative"/></a>
                         </div>
                         <div className="col">
-                            <a href="https://www.github.com"><img src="github.png" alt="" className=" position-relative"/></a>
+                            <a href="https://github.com/RichardIrala" target="_blank"><img src="github.png" alt="" className=" position-relative"/></a>
                         </div>
                         <div className="col"></div>
                       </div>
