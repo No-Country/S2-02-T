e9c6097f3885a9fa801f17fec0fd16eaf4c6e1c5
F4cku23 <f4cku23.n@outlook.es>
2022-06-17 22:50:49 -0300

    se agrego clase "col-sm" para el responsive

--- react-app/src/components/SobreNosotros/SobreNosotros.jsx
+++ react-app/src/components/SobreNosotros/SobreNosotros.jsx
@@ -19,14 +19,14 @@ class SobreNosotros extends React.Component {
                       <div className="card-body">
                       <h5 className="card-title text-xl-center">Paulo Nievas - Backend</h5>
                       <div className="row">
-                      <div className="col"></div>
-                        <div className="col">
+                      <div className="col col-sm"></div>
+                        <div className="col col-sm-4">
                           <a href="https://www.linkedin.com/in/paulo-facundo-gaston-nievas/" target="_blank"><img src="linkedin.png" alt="" className=" position-relative"/></a>
                         </div>
-                        <div className="col">
+                        <div className="col col-sm-4">
                             <a href="https://github.com/F4cku23" target="_blank"><img src="github.png" alt="" className=" position-relative"/></a>
                         </div>
-                        <div className="col"></div>
+                        <div className="col col-sm"></div>
                       </div>
                       </div>
                   </div>
@@ -35,16 +35,16 @@ class SobreNosotros extends React.Component {
                   <div className="card">
                       <img className="card-img-top" src="orlando_neris.jfif" alt="perfil"/>
                       <div className="card-body">
-                      <h5 className="card-title text-xl-center">Orlando - Backend</h5>
+                      <h5 className="card-title text-xl-center">Orlando Neris - Backend</h5>
                       <div className="row">
-                      <div className="col"></div>
-                        <div className="col">
+                      <div className="col col-sm"></div>
+                        <div className="col col-sm-4">
                           <a href="https://www.linkedin.com/in/orlandoneris823/" target="_blank"><img src="linkedin.png" alt="" className=" position-relative"/></a>
                         </div>
-                        <div className="col">
+                        <div className="col col-sm-4">
                             <a href="https://github.com/OlandoNeris" target="_blank"><img src="github.png" alt="" className=" position-relative"/></a>
                         </div>
-                        <div className="col"></div>
+                        <div className="col col-sm"></div>
                       </div>
                       </div>
                   </div>
@@ -53,16 +53,16 @@ class SobreNosotros extends React.Component {
                   <div className="card">
                       <img className="card-img-top" src="oscar_ruiz.jfif" alt="perfil"/>
                       <div className="card-body">
-                      <h5 className="card-title text-xl-center">Oscar - Backend</h5>
+                      <h5 className="card-title text-xl-center">Oscar Ruiz - Backend</h5>
                       <div className="row">
-                      <div className="col"></div>
-                        <div className="col">
+                      <div className="col col-sm"></div>
+                        <div className="col col-sm-4">
                           <a href="https://www.linkedin.com/in/oscar-ruiz-7a3371171/" target="_blank"><img src="linkedin.png" alt="" className=" position-relative"/></a>
                         </div>
-                        <div className="col">
+                        <div className="col col-sm-4">
                             <a href="https://github.com/dert98" target="_blank"><img src="github.png" alt="" className=" position-relative"/></a>
                         </div>
-                        <div className="col"></div>
+                        <div className="col col-sm"></div>
                       </div>
                       </div>
                   </div>
@@ -78,16 +78,16 @@ class SobreNosotros extends React.Component {
                   <div className="card">
                       <img className="card-img-top" src="julian_perna.jfif" alt="perfil"/>
                       <div className="card-body">
-                      <h5 className="card-title text-xl-center">Julian - Frontend</h5>
+                      <h5 className="card-title text-xl-center">Julián Perna - Frontend</h5>
                       <div className="row">
-                      <div className="col"></div>
-                        <div className="col">
+                      <div className="col col-sm"></div>
+                        <div className="col col-sm-4">
                           <a href="https://www.linkedin.com/in/julián-perna-5b7508136/" target="_blank"><img src="linkedin.png" alt="" className=" position-relative"/></a>
                         </div>
-                        <div className="col">
+                        <div className="col col-sm-4">
                             <a href="https://github.com/julianperna" target="_blank"><img src="github.png" alt="" className=" position-relative"/></a>
                         </div>
-                        <div className="col"></div>
+                        <div className="col col-sm"></div>
                       </div>
                       </div>
                   </div>
@@ -96,16 +96,16 @@ class SobreNosotros extends React.Component {
                   <div className="card">
                       <img className="card-img-top" src="richard_irala.jfif" alt="perfil"/>
                       <div className="card-body">
-                      <h5 className="card-title text-xl-center">Richard - Frontend</h5>
+                      <h5 className="card-title text-xl-center">Richard Irala - Frontend</h5>
                       <div className="row">
-                      <div className="col"></div>
-                        <div className="col">
+                      <div className="col col-sm"></div>
+                        <div className="col col-sm-4">
                           <a href="https://www.linkedin.com/in/richard-miguel-irala-5139a2217/" target="_blank"><img src="linkedin.png" alt="" className=" position-relative"/></a>
                         </div>
-                        <div className="col">
+                        <div className="col col-sm-4">
                             <a href="https://github.com/RichardIrala" target="_blank"><img src="github.png" alt="" className=" position-relative"/></a>
                         </div>
-                        <div className="col"></div>
+                        <div className="col col-sm"></div>
                       </div>
                       </div>
                   </div>
