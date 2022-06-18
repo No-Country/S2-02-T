8306421348b434fccbcde2027add686090933a81
Julián Perna <julianperna@gmail.com>
2022-06-18 15:20:52 -0300

    hubo correcciones en el hero

--- react-app/src/components/Hero/Hero.js
+++ react-app/src/components/Hero/Hero.js
@@ -1,12 +1,13 @@
 import "./styles.css"
 
+
 const Hero = () => {
   return (
     <>
       <div className="hero__container">
         <h1 className="hero__title">Digital Finance</h1>
         <p className="hero__subtitle">La mejor calculadora para simular tus gastos. Facil y rápido!</p>
-        <div className="hero__button">Probar</div>
+       <a href="#calculadora"className="hero__button" >Probar</a> 
         <div className="hero__image">
           <img className="hero__calculator" src="calcu-money.jpeg" alt="" />
         </div>
